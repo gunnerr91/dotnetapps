8792412d62744a6e07e5baad44312887be269ac4
gunnerr91 <blissvortex@hotmail.com>
2021-06-19 15:14:33 +0100

    moved into seperate folder, added test project

---
+++ CQRSDemo/CQRSDemoTests/Features/ProductFeatures/Queries/GetAllProductsQueryTests.cs
@@ -0,0 +1,27 @@
+﻿using CQRSDemo.Context;
+using CQRSDemo.Features.ProductFeatures.Queries;
+using CQRSDemo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using static CQRSDemo.Features.ProductFeatures.Queries.GetAllProductsQuery;
+
+namespace CQRSDemoTests.Features.ProductFeatures.Queries
+{
+    public class GetAllProductsQueryTests : TestHelper
+    {
+        [Fact]
+        public async Task Retrieves_All_Products()
+        {
+            appDbContext.Products.AddRange(new Product(), new Product());
+            await appDbContext.SaveChanges();
+            var requestQuery = new GetAllProductsQueryHandler(appDbContext);
+            var result = await requestQuery.Handle(new GetAllProductsQuery(), new System.Threading.CancellationToken());
+            Assert.Equal(2, result.Count());
+        }
+    }
+}
---
+++ CQRSDemo/CQRSDemoTests/TestHelper.cs
@@ -0,0 +1,26 @@
+﻿using CQRSDemo.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRSDemoTests
+{
+    public class TestHelper
+    {
+        protected readonly ApplicationContext appDbContext;
+
+        public TestHelper()
+        {
+            var builder = new DbContextOptionsBuilder<ApplicationContext>();
+            builder.UseInMemoryDatabase(databaseName: "AppContextDbInMemory");
+
+            var dbContextOptions = builder.Options;
+            appDbContext = new ApplicationContext(dbContextOptions);
+            appDbContext.Database.EnsureDeleted();
+            appDbContext.Database.EnsureCreated();
+        }
+    }
+}
